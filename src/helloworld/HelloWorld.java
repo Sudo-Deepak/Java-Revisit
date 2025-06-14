c3e75d8a3ccd879cc4251920ad8a41b1ec600ad9
Sudo-Deepak <deepakprajapati.officials@gmail.com>
2025-06-14 19:12:44 +0530

    A first Program "Hello World"

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -1,6 +1,14 @@
 package helloworld;
 
+/**
+ * A first Java Program.
+ */
 public class HelloWorld {
+    /**
+     * Entry point of program. JVM will execute the main method.
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
